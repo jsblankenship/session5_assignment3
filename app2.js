b97b61bc899972819e480c10a4a18807df5ac738
Jeff Blankenship <jsblankenship44@gmail.com>
2017-04-04 15:24:42 -0400

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+var employee = {	firstName:"Jeff",
+					age:"42" ,
+					salary:"54000" ,
+					address:{
+						city:"Charleston" ,
+						state:"West Virginia",
+						pincode:"25301",
+					},
+}
+
+console.log("The name is " + employee.firstName);
+console.log("age  is "+ employee.age);
+console.log("Salary is " + employee.salary);
+console.log("address is " + employee.address.city +  employee.address.state +  employee.address.pincode);
